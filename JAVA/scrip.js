35dae29cce1f9f5889f2bee96a5c7a571dc918ce
Emmannuel22 <isaiasbernal831@gmail.com>
2025-06-06 17:46:44 -0600

    Primer commit

---
+++ JAVA/scrip.js
@@ -0,0 +1,11 @@
+console.log("hola")
+
+
+
+document.querySelector('.menu-btn').addEventListener('click', () =>{ 
+    //document.querySelector('.nav-menu').classList.toggle('show');
+    let prueba = document.querySelector('.nav_menu')
+    console.log(prueba);
+    prueba.classList.toggle('show');
+    console.log("hola");
+});
